18706c712841abc459cf35e93b9dff52c7481cdd
ajboricua <60121695+ajboricua@users.noreply.github.com>
2021-07-10 19:34:59 -0400

    added new style and js

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+
+// html styling inserts
+var div = document.createElement("#quiz")
+    div.id = 'header'
+    div.innerHTML = '<h2>Coding Quiz Challenge</h2>';
+    document.body.appendChild("quiz");
+
+     h2 = document.getElementById("h2")
+
+// timer countdown
+var = timerCountdown.createElement()
+
+// 
+var startBTN = document.querySelector("#startBTN")
+
+var timerCountdown = function(event) {
+    event.preventDefault();
+
+
+};
+
+document.querySelector
+
+
+// Saves data to localStorage
+// localStorage.setItem()
