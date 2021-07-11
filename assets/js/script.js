773c21fb9d5cc75b8a5c040d25c3b27cc0a13045
ajboricua <60121695+ajboricua@users.noreply.github.com>
2021-07-11 14:46:29 -0400

    javascript changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,87 +19,87 @@ body.appendChild(pEl);
 // button for documentation
 var button = document.createElement("button");
 button.innerHTML = "Start Quiz";
+button.id = "button"
 
-// 2. Append somewhere
+// 2. body append
 var body = document.getElementsByTagName("body")[0];
 body.appendChild(button);
 
 // 3. style 
 button.setAttribute('style', 'width:50%; margin-left:25%; margin-right:25%;');
 
-// 3. Add event handler
-button.addEventListener ("click", function() {
+// 4. Add event handler
+button.addEventListener ("click", function(countDown) {
   alert("The Quiz has started!");
 });
 
+// countdown
+var timerEl = document.getElementsByClassName("countDown");
+// startbtn 
+var button = document.getElementById("button");
 
-// The array of questions for the game.
-var questions = [
-    { q: 'The sky is blue.', a: 't' },
-    { q: 'There are 365 days in a year.', a: 't' },
-    { q: 'There are 42 ounces in a pound.', a: 'f' },
-    { q: 'The Declaration of Independence was created in 1745.', a: 'f' },
-    { q: 'Bananas are vegetables.', a: 'f' }
-  ];
-  
-  // We start the game with a score of 0.
-  var score = 0;
-  
-  // Loop over every question object
-  for (var i = 0; i < questions.length; i++) {
-    // Display current question to user and ask OK/Cancel
-    var answer = confirm(questions[i].q);
-  
-    // Compare answers
-    if (
-      (answer === true && questions[i].a === 't') ||
-      (answer === false && questions[i].a === 'f')
-    ) {
-      // Increase score
-      score++;
-      // Alert the user
-      alert('Correct!');
+function countDown(timerEl, button) {
+    var timeLeft = 60;
+    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+    var timeInterval = setInterval(function() {
+    // As long as the `timeLeft` is greater than 1
+    if (timeLeft > 1) {
+      // Set the `textContent` of `timerEl` to show the remaining seconds
+      timerEl.textContent = timeLeft + ' seconds remaining';
+      // Decrement `timeLeft` by 1
+      timeLeft--;
+    } else if (timeLeft === 1) {
+      // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
+      timerEl.textContent = timeLeft + ' second remaining';
+      timeLeft--;
     } else {
-      alert('Wrong!');
+      // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+      timerEl.textContent = '';
+      // Use `clearInterval()` to stop the timer
+      clearInterval(timeInterval);
+      // Call the `displayMessage()` function
+      
+    window.onload = function() {
+        var oneMinute = 60 * 1,
+            display = document.querySelector('#button');
     }
-  }
+
+button.onclick = countDown;
+console.log();
+    };
+
+
+
+
+// // The array of questions for the game.
+// function startBtn(){
+
+// var questions = [
+//     { q: 'The sky is blue.', a: 't' },
+//     { q: 'There are 365 days in a year.', a: 't' },
+//     { q: 'There are 42 ounces in a pound.', a: 'f' },
+//     { q: 'The Declaration of Independence was created in 1745.', a: 'f' },
+//     { q: 'Bananas are vegetables.', a: 'f' }
+//   ];
   
-  // Show total at end
-  alert('You got ' + score + '/' + questions.length);
-
-
-
-
-
-
-
-// var startBtn = document.getElementById('start');
-// var timerEl = document.getElementById('countdown');
-// var mainEl = document.getElementById('main');
-
-
-// // html styling inserts
-// var div = document.createElement("#quiz")
-//     div.id = 'header'
-//     div.innerHTML = '<h2>Coding Quiz Challenge</h2>';
-//     document.body.appendChild("quiz");
-
-//      h2 = document.getElementById("h2")
-
-// // timer countdown
-// timerEl = timerCountdown.createElement("timer")
-
-// // 
-// var startBTN = document.querySelector("#startBTN")
-
-// var timerCountdown = function(event) {
-//     event.preventDefault();
-
-
-// };
-
-// document.querySelector
-
-
-// // Saves data to localStorage
-// // localStorage.setItem()
+//   // We start the game with a score of 0.
+//   var score = 0;
+  
+//   // Loop over every question object
+//   for (var i = 0; i < questions.length; i++) {
+//     // Display current question to user and ask OK/Cancel
+//     var answer = confirm(questions[i].q);
+  
+//     // Compare answers
+//     if (
+//       (answer === true && questions[i].a === 't') ||
+//       (answer === false && questions[i].a === 'f')
+//     ) {
+//       // Increase score
+//       score++;
+//       // Alert the user
+//       alert('Correct!');
+//     } else {
+//       alert('Wrong!');
+//   
+    };
