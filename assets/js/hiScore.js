974454b583ca5dc7aa31177dd7e7b63a739d254e
Tony Rivera <ajboricua@gmail.com>
2021-07-24 17:14:59 -0400

    update everything

---
+++ assets/js/hiScore.js
@@ -0,0 +1,18 @@
+var highScore = 
+
+    // localstorage
+        // save the data on local storage
+            localStorage.setItem()
+            // getelementbyid("#") 
+
+        // fetch the hiscore
+            localStorage.getItem()
+
+        // clear hiscore
+        localStorage.removeItem()
+        
+
+        // append
+        hiscore.appendchild()
+
+        
